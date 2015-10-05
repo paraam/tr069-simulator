15ce987533dea6d2e137c38c089b42132804c94e
Vunb <nhubaovu@gmail.com>
2015-10-06 02:07:03 +0700

    add xml binding: NoMoreRequests class

---
+++ src/main/java/org/dslforum/cwmp_1_0/NoMoreRequests.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dslforum.cwmp_1_0;
+
+/**
+ *
+ * @author Vunb
+ */
+public class NoMoreRequests {
+
+    private String string;
+
+    /**
+     * Get the extension value.
+     *
+     * @return value
+     */
+    public String getString() {
+        return string;
+    }
+
+    /**
+     * Set the extension value.
+     *
+     * @param string
+     */
+    public void setString(String string) {
+        this.string = string;
+    }
+}
